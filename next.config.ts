4496bb8031b00c0f24bbfe9eeab8cc7e062e80f0
Berat Ciftci <beratciftciofficial@gmail.com>
2026-01-28 22:38:08 +0300

    Remove forbidden eslint config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
-    // TypeScript hatalarını görmezden gel (Bu hala geçerli)
     ignoreBuildErrors: true,
   },
 };
