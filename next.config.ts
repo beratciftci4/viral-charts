69526413a7105fd1de04bb68e9ae0efbd6295b2d
Berat Ciftci <beratciftciofficial@gmail.com>
2026-01-28 23:07:50 +0300

    Fix config for Next.js 16

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Sadece TypeScript hatalarını yoksayıyoruz.
-  // ESLint ayarını buradan kaldırdık çünkü v16 bunu kabul etmiyor.
+  // Sadece TypeScript hatalarını görmezden geliyoruz
   typescript: {
     ignoreBuildErrors: true,
   },
-  // Eğer linting hatası alırsan, proje kökünde .eslintignore dosyası oluşturup içine * koyman yeterli.
 };
 
 export default nextConfig;
